141e623cdf62fa76e0a71acc37244f478bbc6948
gyeompain <kimhg1997@gmail.com>
2024-04-13 12:50:45 +0900

    logback xml 추가 로그인 시스템 추가

--- GoalTech/src/main/java/com/goaltech/login/web/LoginController.java
+++ GoalTech/src/main/java/com/goaltech/login/web/LoginController.java
@@ -2,15 +2,14 @@ package com.goaltech.login.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.goaltech.login.service.LoginService;
 import com.goaltech.login.vo.UserVO;
@@ -52,7 +51,6 @@ public class LoginController {
 		vo.setUser_phone(phone);
 
 		//3. userVO에 Insert하기
-		Object hi;
 		loginService.insertUser(vo);
 		model.addAttribute(vo);
 		return "main";
@@ -70,32 +68,37 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="login_proc.do", method= RequestMethod.POST)
-	public String loginProc(HttpServletRequest request) throws Exception {
+	public String loginProc(HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
 		
 		//1. 사용자 입력 정보 추출
 		String id = request.getParameter("user_id");
 		String pw = request.getParameter("user_pw");
 		
-		if(!isValidInput(id, pw)) {
+		/*if(!isValidInput(id, pw)) {
 			return "login";
 		}
+		*/
 		
 		//2. DB 연동 처리
 		UserVO userVO = new UserVO();
 		userVO.setUser_id(id);
 		userVO.setUser_pw(pw);
 		
+		
+		
 		//3. 사용자 인증
 		UserVO authenticatedUser = loginService.selectUser(userVO);
 		if(null!=authenticatedUser.getUser_name() && authenticatedUser !=null) {
+			
 			System.out.println("로그인 후 메인 페이지로 이동");
-			return "redirect:/main";
+			return "main";
 		}else {
 			System.out.println("로그인 화면으로 이동");
 			return "login";			
 		}
 
 	}
+	
 	
 	private boolean isValidInput(String id, String pw) {
 		
